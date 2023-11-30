3cc2d2c89bbaa11dbf7da8652d0b96e9a84a395d
Amarok <91077200+AmarokIce@users.noreply.github.com>
2023-11-30 14:32:33 +0800

    edit

--- src/main/java/club/someoneice/sol/EventPlayerEntity.java
+++ src/main/java/club/someoneice/sol/EventPlayerEntity.java
@@ -1,11 +1,15 @@
 package club.someoneice.sol;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ChatComponentTranslation;
@@ -13,6 +17,9 @@ import net.minecraft.util.FoodStats;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Random;
 
 public class EventPlayerEntity {
@@ -28,11 +35,28 @@ public class EventPlayerEntity {
         EntityPlayer player = event.entityPlayer;
         NBTTagCompound nbt = player.getEntityData();
 
+        int[] foodsIds;
+        if (nbt.hasKey("foodsIds")) {
+            foodsIds = nbt.getIntArray("foodsIds");
+        } else foodsIds = new int[13];
+
+        ItemFood itemFood = (ItemFood) item.getItem();
+        FoodStats foodStats = player.getFoodStats();
+        Random random = new Random();
+
+        if (checkFoodCount(foodsIds, Item.getIdFromItem(itemFood)) > 5) {
+            foodStats.setFoodLevel(Math.max(foodStats.getFoodLevel() - (itemFood.func_150905_g(item) / 2), 0));
+            foodStats.setFoodSaturationLevel(Math.max(foodStats.getSaturationLevel() - (itemFood.func_150906_h(item) / 2), 0));
+        } else if (checkFoodCount(foodsIds, Item.getIdFromItem(itemFood)) > 10) {
+            foodStats.setFoodLevel(Math.max(foodStats.getFoodLevel() - itemFood.func_150905_g(item), 0));
+            foodStats.setFoodSaturationLevel(Math.max(foodStats.getSaturationLevel() - itemFood.func_150906_h(item), 0));
+
+            return;
+        }
+
         int mealValue = nbt.getInteger(meal);
         int meatValue = nbt.getInteger(meat);
         int veritableValue = nbt.getInteger(veritable);
-
-        Item itemFood = item.getItem();
 
         boolean isMealFlag = ItemTag.MEAL.has(itemFood);
         boolean isMeatFlag = ItemTag.MEAT.has(itemFood);
@@ -53,9 +77,6 @@ public class EventPlayerEntity {
             veritableValue += 2;
             meatValue = Math.max(meatValue - 1, 0);
         }
-
-        FoodStats foodStats = player.getFoodStats();
-        Random random = new Random();
 
         if (meatValue > 50) {
             if (isMeatFlag) {
@@ -111,6 +132,8 @@ public class EventPlayerEntity {
         nbt.setInteger(meal, mealValue);
         nbt.setInteger(meat, meatValue);
         nbt.setInteger(veritable, veritableValue);
+
+        nbt.setIntArray("foodsIds", addFoodInList(foodsIds, Item.getIdFromItem(itemFood)));
     }
 
     @SubscribeEvent
@@ -125,5 +148,24 @@ public class EventPlayerEntity {
         nbt.setInteger(meal, Math.max(mealValue - 10, 0));
         nbt.setInteger(meat, Math.max(meatValue - 10, 0));
         nbt.setInteger(veritable, Math.max(veritableValue - 10, 0));
+    }
+
+    private int checkFoodCount(int[] foodsIds, int foodId) {
+        Map<Integer, Integer> idMapping = Maps.newHashMap();
+        for (int i = 0; i < 13; i ++) {
+            idMapping.put(foodsIds[i], idMapping.getOrDefault(foodsIds[i], 1));
+        }
+
+        if (idMapping.containsKey(foodId)) return idMapping.get(foodId);
+        else return -1;
+    }
+
+    private int[] addFoodInList(int[] foodsIds, int foodId) {
+        int[] newIds = new int[13];
+        newIds[0] = foodId;
+        for (int i = 0; i < 12; i ++) {
+            newIds[i + 1] = foodsIds[i];
+        }
+        return newIds;
     }
 }
